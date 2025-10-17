29d670514a5e8ba3abd993ae6d175a0d95ef2069
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-10-17 18:19:25 -0500

    adding new api

---
+++ alembic/versions/7902f9d1f5bb_add_all_enhancements.py
@@ -0,0 +1,24 @@
+"""add_all_enhancements
+
+Revision ID: 7902f9d1f5bb
+Revises: 136432d7cf5c
+Create Date: 2025-10-17 18:18:48.454064
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7902f9d1f5bb'
+down_revision = '136432d7cf5c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
